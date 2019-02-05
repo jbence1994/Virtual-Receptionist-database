a60168711beb7951d5b3200e9b87e44449cc00c0
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-02-05 12:36:11 +0100

    Create guest_testdata.sql

---
+++ guest_testdata.sql
@@ -0,0 +1,4 @@
+INSERT INTO `guest` (`id`, `name`, `documentnumber`, `citizenship`, `birthdate`, `vatnumber`, `country`, `zipcode`, `city`, `address`, `phonenumber`, `emailaddress`)
+VALUES (NULL, 'Juhász Bence', '134573AE', 'magyar', '1994-03-27', '', '111', '6900', 'Makó', 'Kálvária utca 48./A', '06 (20) / 294-4280', 'juhasz.bence@outlook.hu'),
+(NULL, 'Autóscsárda-Panzió Kft.', '', '', NULL, '13542199-2-06', '111', '6900', 'Makó', 'Deák Ferenc utca 28./B', '06 (62) / 510-298', 'info@autospanzio.hu'),
+(NULL, 'Duna-Döner Kft.', '', '', NULL, '14217395-2-06', '111', '6900', 'Makó', 'Hunyadi u. 4./A', '06 626 38 225', 'office@dunadoner.com');
