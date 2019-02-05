369f624ae9492bc816e397ca7cf75bfe467417ac
jbence1994 <33096954+jbence1994@users.noreply.github.com>
2019-02-05 12:51:12 +0100

    Update guest_testdata.sql

--- guest_testdata.sql
+++ guest_testdata.sql
@@ -1,3 +1,5 @@
+TRUNCATE TABLE `guest`;
+
 INSERT INTO `guest` (`id`, `name`, `documentnumber`, `citizenship`, `birthdate`, `vatnumber`, `country`, `zipcode`, `city`, `address`, `phonenumber`, `emailaddress`)
 VALUES (NULL, 'Juhász Bence', '134573AE', 'magyar', '1994-03-27', '', '111', '6900', 'Makó', 'Kálvária utca 48./A', '06 (20) / 294-4280', 'juhasz.bence@outlook.hu'),
 (NULL, 'Autóscsárda-Panzió Kft.', '', '', NULL, '13542199-2-06', '111', '6900', 'Makó', 'Deák Ferenc utca 28./B', '06 (62) / 510-298', 'info@autospanzio.hu'),
